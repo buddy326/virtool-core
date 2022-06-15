bddcbfe6658f29ec578732645a32b8daf9264c6a
Aman Monga <aman260@outlook.com>
2022-06-15 15:33:39 -0700

    feat: add history data models

--- virtool_core/models/enums.py
+++ virtool_core/models/enums.py
@@ -10,3 +10,20 @@ class Permission(Enum):
     remove_file = "remove_file"
     remove_job = "remove_job"
     upload_file = "upload_file"
+
+
+class HistoryMethod(Enum):
+    add_isolate = "add_isolate"
+    clone = "clone"
+    create_sequence = "create_sequence"
+    create = "create"
+    edit = "edit"
+    edit_isolate = "edit_isolate"
+    edit_sequence = "edit_sequence"
+    import_otu = "import"
+    remove = "remove"
+    remove_isolate = "remove_isolate"
+    remove_sequence = "remove_sequence"
+    remote = "remote"
+    set_as_default = "set_as_default"
+    update = "update"
